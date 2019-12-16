97a3461a369959d4fb7c4eb501b2405e96e39111
Shehata Haroun <hshehata@edu.lapinamk.fi>
2019-12-16 09:58:16 +0200

    Added TableLayout

--- app/src/main/java/com/example/myfirstapp/NewActivity.java
+++ app/src/main/java/com/example/myfirstapp/NewActivity.java
@@ -2,8 +2,6 @@ package com.example.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
